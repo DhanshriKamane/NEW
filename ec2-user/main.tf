7ca716802831692d4ac495a1f066650258cf7e01
dhanu <dhanukamane11@gmail.com>
2024-07-02 00:09:33 +0530

    change

---
+++ ec2-user/main.tf
@@ -0,0 +1,14 @@
+# provider "aws" {
+#   region = "ap-south-1"
+# }
+
+resource "aws_instance" "name" {
+    ami = "ami-04f8d7ed2f1a54b14"
+    instance_type = "t2.nano"
+    # key_name = "jenkin"
+
+    tags = {
+      Name = "newdev"
+    }
+  
+}
---
+++ s3-dyano-config/main.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "dev" {
+    bucket = "statefile-configure-dhanu"
+  
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.dev.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
